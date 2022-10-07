afc56dba6ab38a74698be26a3e5ad5f1ede9dd65
Clemens Oertel <clemens.oertel@yoppworks.com>
2022-10-07 18:12:53 -0400

    Address bot issues

--- Formula/riddlc.rb
+++ Formula/riddlc.rb
@@ -1,17 +1,15 @@
-require "formula"
 
 class Riddlc < Formula
-  desc "RIDDL is a language and toolset for specifying a system design."
+  desc "RIDDL is a language and toolset for specifying a system design"
   homepage "https://riddl.tech/"
   url "https://github.com/reactific/riddl/releases/download/0.15.3/riddlc-0.15.3.zip"
   sha256 "ee993a6703440a387b087824dbb68165a250fa30883ae408fe9f1e8bca01bcb0"
-  license "Apache License 2.0"
+  license "Apache-2.0"
 
   depends_on "openjdk"
 
   def install
-      # inreplace "bin/riddlc", "##PREFIX##", "#{prefix}/lib"
-      lib.install Dir["lib/*"]
-      bin.install "bin/riddlc"
+    lib.install Dir["lib/*"]
+    bin.install "bin/riddlc"
   end
 end
